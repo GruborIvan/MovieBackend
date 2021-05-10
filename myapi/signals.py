7d6aed58af723b40a80d2cf5d9076c342eb2829f
Ivan <ivan.grubor@gmail.com>
2021-05-10 15:46:04 +0200

    #CSCM-81/Move-EmailSending-To-Queue-Done

--- myapi/signals.py
+++ myapi/signals.py
@@ -1,23 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Movie
-from socket import gaierror
-from django.core.mail import send_mail
-from django.conf import settings
+from .tasks import *
 
 @receiver(post_save,sender=Movie)
 def send_email_confirmation(sender,instance,created,**kwargs):
     if created:
-        try:
-            send_mail(
-                'A new movie is added to the system!',
-                'Movie added to movie database: \n Title: {} \n Description: {}'.format(instance.title,instance.description),
-                settings.EMAIL_HOST_USER,
-                settings.EMAIL_RECIEVERS,
-                fail_silently=False,
-            )
-            print('Email has been sent!')
-        except (gaierror,ConnectionRefusedError):
-            print('Failed to connect to server! Probably bad connection settings.')
-        except:
-            print('Error! Unable to send movie confirmation email!')
+        send_movie_email.delay(instance.title,instance.description)
---
+++ myapi/tasks.py
@@ -0,0 +1,22 @@
+from celery.decorators import task
+from socket import gaierror
+from django.core.mail import send_mail
+from django.conf import settings
+from project.celery.celery import app
+from celery.exceptions import SoftTimeLimitExceeded
+from django.core.exceptions import BadRequest
+
+@app.task(name="send_email",autoretry_for=(BadRequest,SoftTimeLimitExceeded))
+def send_movie_email(title,description):
+    try:
+        send_mail(
+            'A new movie is added to the system!',
+            'Movie added to movie database: \n Title: {} \n Description: {}'.format(title,description),
+            settings.EMAIL_HOST_USER,
+            settings.EMAIL_RECIEVERS,
+            fail_silently=False,
+        )
+        print('Email has been sent!')
+
+    except Exception:
+        print('Error! Unable to send movie confirmation email!')
